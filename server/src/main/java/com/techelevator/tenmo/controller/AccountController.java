c1e4aeed39c4a4be93ae2b95d6cce0c4c81c1390
Nathanael Aninweze <nathanael.aninweze@outlook.com>
2023-04-26 16:59:28 -0400

    My commit

---
+++ server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -0,0 +1,49 @@
+package com.techelevator.tenmo.controller;
+
+import com.techelevator.tenmo.dao.JdbcAccountDao;
+import com.techelevator.tenmo.dao.JdbcUserDao;
+import com.techelevator.tenmo.model.Account;
+import com.techelevator.tenmo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.math.BigDecimal;
+import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/account")
+@PreAuthorize("isAuthenticated()")
+public class AccountController {
+
+    @Autowired
+    private JdbcAccountDao jdbcAccountDao;
+    @Autowired
+    private JdbcUserDao jdbcUserDao;
+
+
+    @RequestMapping("/balance")
+    public BigDecimal getAccount(Principal principal){
+
+        Account account  =jdbcAccountDao.getAccountByUserName(principal.getName());
+       if(account !=null){
+           BigDecimal balance = account.getBalance();
+           return account.getBalance();
+       }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND,"userId not found");
+    }
+    @GetMapping("/listUsers")
+    public List<String> list(Principal principal){
+
+        return jdbcUserDao.findAll().stream()
+                .map(User::getUsername)
+                .filter(username -> !username.equals(principal.getName()))
+                .collect(Collectors.toList());
+    }
+}
--- server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -2,7 +2,11 @@ package com.techelevator.tenmo.dao;
 
 import com.techelevator.tenmo.model.Account;
 
+import java.math.BigDecimal;
+
 public interface AccountDao {
     public Account getAccountByUserId(int id);
     public boolean createAccount(int userId);
+    public BigDecimal getBalance(int userId);
+    public Account getAccountByUserName(String userName);
 }
